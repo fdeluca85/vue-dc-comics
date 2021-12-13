72075f940542249695671f896cc40d4fc6fdc7bf
francesca <fradeluca85@gmail.com>
2021-12-13 16:47:00 +0100

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,24 @@
 <template>
     <header>
-        <div class="logo">
+        <div class="container">
+            <div class="logo">
         <img src="../assets/img/dc-logo.png" alt="logo-DC">
     </div>
     <nav>
         <ul>
-            <li><a href="#"></a>Characters</li>
-            <li><a href="#"></a>Comics</li>
-            <li><a href="#"></a>Movies</li>
-            <li><a href="#"></a>TV</li>
-            <li><a href="#"></a>Games</li>
-            <li><a href="#"></a>Collectibles</li>
-            <li><a href="#"></a>Videos</li>
-            <li><a href="#"></a>Fans</li>
-            <li><a href="#"></a>News</li>
-            <li><a href="#"></a>Shop</li>            
+            <li><a href="#">Characters</a></li>
+            <li><a class="active" href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Collectibles</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Fans</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Shop</a></li>            
         </ul>
     </nav>
+        </div>
     </header>
   
 </template>
@@ -28,15 +30,39 @@ export default {
 }
 </script>
 
-<style scopes>
+<style lang="scss" scopes>
 header{
-    display: flex;
-    justify-content: space-between;
+    .container{
+        display: flex;
+        justify-content: space-between;
+        margin-top: 20px;
+    }
 }
-li{
-    text-transform: uppercase;
-    list-style: none;
-    display: flex;
+nav{
+    ul{        
+        margin-top: 20px;
+        text-transform: uppercase;
+        list-style: none;
+        display: flex;
+    }
+    li a{
+        display: inline-block;
+        padding: 20px 5px;
+        margin: 0 10px;
+        color: black;
+        font-size: 12px;
+        font-weight: 700;
+        text-decoration: none;
+
+        &.active{
+            // background-color: yellow;
+            color: #0C7CEC;
+            border-bottom: 5px solid #0C7CEC;
+        }
+    }
+}
+img{
+    width: 80%;
 }
 
 </style>
